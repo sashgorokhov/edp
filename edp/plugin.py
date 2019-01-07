52efb372b8c8aa5d3c6494c15e0f83e4ef0ad423
Alexander Gorokhov <sashgorokhov@gmail.com>
2019-01-08 01:25:01 +0300

    Plugin callbacks

--- edp/plugin.py
+++ edp/plugin.py
@@ -1,10 +1,21 @@
 import abc
+import collections
 import importlib.util
 import logging
 import pathlib
-from typing import List, Type, Iterator
+import inspect
+from typing import List, Type, Iterator, Mapping, Callable, Any
 
 logger = logging.getLogger(__name__)
+
+
+def callback(name=None):
+    def decor(func):
+        nonlocal name
+        name = name or func.__name__
+        func.__is_callback__ = name
+        return func
+    return decor
 
 
 class BasePlugin(metaclass=abc.ABCMeta):
@@ -22,6 +33,7 @@ class PluginManager:
     def __init__(self, base_dir: pathlib.Path):
         self._base_dir = base_dir
         self._plugins: List[BasePlugin] = []
+        self._callbacks: Mapping[str, List[Callable]] = collections.defaultdict(list)
 
     def load_plugins(self):
         for path in self._base_dir.iterdir():
@@ -47,6 +59,7 @@ class PluginManager:
                 try:
                     plugin = self._init_plugin(cls)
                     self._plugins.append(plugin)
+                    self._register_callbacks(plugin)
                 except:
                     logger.exception('Failed to initialize plugin %s from %s', cls, path)
 
@@ -58,3 +71,25 @@ class PluginManager:
 
     def _init_plugin(self, cls: Type[BasePlugin]) -> BasePlugin:
         return cls()
+
+    def _register_callbacks(self, plugin: BasePlugin):
+        for name, t, cls, value in inspect.classify_class_attrs(plugin.__class__):
+            if t != 'method' or not hasattr(value, '__is_callback__'):
+                continue
+            callback_name = getattr(value, '__is_callback__')
+            self._callbacks[callback_name].append(value.__get__(plugin, cls))
+            logger.debug('Registered callback "%s" of %s', callback_name, plugin)
+
+    def emit(self, name, **kwargs) -> List[Any]:
+        logger.debug('Emitting signal: %s', name)
+        results = []
+
+        for func in self._callbacks.get(name, []):
+            try:
+                results.append(func(**kwargs))
+            except:
+                logger.exception('Error executing callback "%s" %s', name, func)
+
+        return results
+
+
