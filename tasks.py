8d9fad7315c6a1ec91cf906884e60f9a8444ad8f
sashgorokhov <sashgorokhov@gmail.com>
2019-01-16 23:16:18 +0300

    build and dist invoke tasks

--- tasks.py
+++ tasks.py
@@ -1,11 +1,23 @@
 from pathlib import Path
 
 import invoke
+import requests
+from invoke import Context
 
 BASE_DIR: Path = Path(__file__).parent
 GUI_DIR: Path = BASE_DIR / 'edp' / 'gui'
 UI_DIR: Path = GUI_DIR / 'ui'
 COMPILED_DIR: Path = GUI_DIR / 'compiled'
+
+UPX_URL = 'https://github.com/upx/upx/releases/download/v3.95/upx-3.95-win64.zip'
+LOCAL_TEMP_DIR: Path = BASE_DIR / '.tmp'
+UPX_DIR: Path = LOCAL_TEMP_DIR / 'upx'
+TARGET: Path = BASE_DIR / 'elite_dangerous_platform.py'
+DIST_DIR: Path = BASE_DIR / 'dist' / TARGET.stem
+DIST_ZIP: Path = DIST_DIR.with_suffix('.zip')
+EXE_FILE: Path = DIST_DIR / TARGET.with_suffix('.exe').name
+
+LOCAL_TEMP_DIR.mkdir(parents=True, exist_ok=True)
 
 
 @invoke.task
@@ -16,3 +28,70 @@ def pyuic(c):
         target_path = COMPILED_DIR / path.with_suffix('.py').name
         print(f'Compiling {path.name} to {target_path}')
         compileUi(str(path), target_path.open('w'))
+
+
+def gethashsum(path, md5=False, sha1=False):
+    import hashlib
+
+    path = Path(path)
+    if not path.exists():
+        raise FileNotFoundError(path)
+    if path.is_dir():
+        raise IsADirectoryError(path)
+
+    if md5:
+        hashobj = hashlib.md5()
+    elif sha1:
+        hashobj = hashlib.sha1()
+    else:
+        return
+
+    with path.open('rb') as f:
+        while True:
+            data = f.read(65536)
+            if not data:
+                break
+            hashobj.update(data)
+
+    hashsum = hashobj.hexdigest()
+    return hashsum
+
+
+def fetch_upx(c: Context):
+    global UPX_DIR
+
+    UPX_ZIP = UPX_DIR.with_suffix('.zip')
+    if UPX_ZIP.exists():
+        UPX_ZIP.unlink()
+    print(f'Fetching UPX form {UPX_URL}')
+    response = requests.get(UPX_URL, stream=True)
+    with UPX_ZIP.open('wb') as f:
+        for chunk in response.iter_content(chunk_size=None):
+            f.write(chunk)
+    c.run(f'powershell Expand-Archive -Force -Path {UPX_ZIP} -DestinationPath {UPX_DIR}')
+    UPX_DIR = next(UPX_DIR.iterdir())
+    print(f'UPX now is in {UPX_DIR}')
+
+
+@invoke.task
+def build(c):
+    if not UPX_DIR.exists():
+        fetch_upx(c)
+
+    c.run(f'pyinstaller -w -y --clean --log-level WARN --upx-dir {UPX_DIR} {TARGET}')
+
+
+@invoke.task(build)
+def dist(c):
+    print(f'Create dist zip in {DIST_ZIP}')
+    c.run(f'powershell Compress-Archive -Force -Path {DIST_DIR} -DestinationPath {DIST_ZIP}')
+
+    md5_exe = gethashsum(EXE_FILE, md5=True)
+    md5_zip = gethashsum(DIST_ZIP, md5=True)
+    sha1_exe = gethashsum(EXE_FILE, sha1=True)
+    sha1_zip = gethashsum(DIST_ZIP, sha1=True)
+
+    print('MD5 exe:', md5_exe)
+    print('MD5 zip:', md5_zip)
+    print('SHA1 exe:', sha1_exe)
+    print('SHA1 zip:', sha1_zip)
