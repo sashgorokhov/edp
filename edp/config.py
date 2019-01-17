58327afe2eb3f7ba530bfe0f5cf1e125c9cc4028
sashgorokhov <sashgorokhov@gmail.com>
2019-01-17 20:56:07 +0300

    Fix config.py BASE_DIR variable

--- edp/config.py
+++ edp/config.py
@@ -4,11 +4,16 @@ from pathlib import Path
 
 # If running from pycharm python console it will be something in pycharm/helpers/pydev....
 EXECUTABLE_PATH: Path = Path(sys.argv[0]).absolute()
-BASE_DIR: Path = EXECUTABLE_PATH.parent
+
+FROZEN: bool = getattr(sys, 'frozen', False)
+
+if FROZEN:
+    BASE_DIR: Path = EXECUTABLE_PATH.parent
+else:
+    BASE_DIR = Path(__file__).parents[1]
+
 LOCALAPPDATA_DIR: Path = Path(os.environ.get('LOCALAPPDATA', BASE_DIR / 'LOCALAPPDATA'))
 SETTINGS_DIR: Path = LOCALAPPDATA_DIR / 'Elite Dangerous Platform' / 'Settings'
 
 VERSION_PATH: Path = BASE_DIR / 'VERSION'
-VERSION: str = VERSION_PATH.read_text().strip() if VERSION_PATH.exists() else '0.0.1'
-
-FROZEN: bool = getattr(sys, 'frozen', False)
+VERSION: str = VERSION_PATH.read_text().strip() if VERSION_PATH.exists() else '0.0.0'
