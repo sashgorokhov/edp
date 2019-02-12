091bfb56ff583ffdeb0f74e1b8e34695fdadc57c
sashgorokhov <sashgorokhov@gmail.com>
2019-02-12 21:21:38 +0300

    Code reformatting according to pylint

--- edp/contrib/overlay_ui.py
+++ edp/contrib/overlay_ui.py
@@ -1,6 +1,5 @@
+"""Overlay UI plugin"""
 from typing import Optional, Dict
-
-from PyQt5 import QtWidgets
 
 from edp import plugins, signals
 from edp.gui.forms import overlay_window
@@ -9,10 +8,12 @@ from edp.settings import BaseSettings
 
 
 class OverlaySettings(BaseSettings):
+    """Overlay plugin settings"""
     layout_widgets: Dict[str, str] = {}  # layout name/id -> widget friendly name
 
 
 class OverlaySettingsTabWidget(VLayoutTab):
+    """Overlay UI settings widget"""
     friendly_name = 'Overlay UI'
 
     def get_settings_links(self):
@@ -20,14 +21,16 @@ class OverlaySettingsTabWidget(VLayoutTab):
 
 
 class OverlayPlugin(plugins.BasePlugin):
+    """Overlay UI plugin"""
     friendly_name = 'Overlay UI'
 
     def __init__(self):
         self.overlay_window: Optional[overlay_window.GameOverlayWindow] = None
 
-    def get_settings_widget(self) -> Optional[QtWidgets.QWidget]:
-        return OverlaySettingsTabWidget()
+    def get_settings_widget(self):
+        return None  # OverlaySettingsTabWidget()
 
     @plugins.bind_signal(signals.app_created)
     def on_app_created(self):
+        """Create overlay window when qt app is created"""
         self.overlay_window = overlay_window.GameOverlayWindow()
